0caa9c279363fd2462fbbbf5eb57f48cb77080c2
6704062616231 <s6704062616231@email.kmutnb.ac.th>
2025-12-14 16:34:31 +0700

    calculate promotion

--- sale.c
+++ sale.c
@@ -2,21 +2,20 @@
 int main(){
     float item1,item2,item3;
     float cal,sum;
-    float vat = 1.07;
 
     scanf("%f",&item1);
     sum = item1;
     cal = sum - (sum * 0.05);
-    printf("Total Price:%.2f\n",cal*vat);
+    printf("Total Price:%.2f\n",cal);
 
     scanf("%f",&item2);
     sum = item1 + item2;
     cal = sum - (sum * 0.15);
-    printf("Total Price:%.2f\n",cal*vat);
+    printf("Total Price:%.2f\n",cal);
 
     scanf("%f",&item3);
     sum = item1 + item2 + item3;
     cal = sum - (sum * 0.30);
-    printf("Total Price:%.2f\n",cal*vat);
+    printf("Total Price:%.2f\n",cal);
     return 0;
 }
