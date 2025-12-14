2f3e5c9606c9682649422d659d1dca87e700079e
6704062616231 <s6704062616231@email.kmutnb.ac.th>
2025-12-14 16:38:42 +0700

    calculate VAT

--- sale.c
+++ sale.c
@@ -2,20 +2,21 @@
 int main(){
     float item1,item2,item3;
     float cal,sum;
+    float vat = 1.07;
 
     scanf("%f",&item1);
     sum = item1;
     cal = sum - (sum * 0.05);
-    printf("Total Price:%.2f\n",cal);
+    printf("Total Price:%.2f\n",cal*vat);
 
     scanf("%f",&item2);
     sum = item1 + item2;
     cal = sum - (sum * 0.15);
-    printf("Total Price:%.2f\n",cal);
+    printf("Total Price:%.2f\n",cal*vat);
 
     scanf("%f",&item3);
     sum = item1 + item2 + item3;
     cal = sum - (sum * 0.30);
-    printf("Total Price:%.2f\n",cal);
+    printf("Total Price:%.2f\n",cal*vat);
     return 0;
 }
